30ebeebefa27b98a24001dcf378a499b5c0f7027
FengAB2 <115358776+FengAB2@users.noreply.github.com>
2023-11-24 17:08:36 +0800

    dense_motion.py

--- src/facerender/modules/dense_motion.py
+++ src/facerender/modules/dense_motion.py
@@ -9,11 +9,32 @@ from src.facerender.sync_batchnorm import SynchronizedBatchNorm3d as BatchNorm3d
 class DenseMotionNetwork(nn.Module):
     """
     Module that predicting a dense motion from sparse motion representation given by kp_source and kp_driving
+    这段代码实现了一个称为 Dense Motion Network 的神经网络模块，用于从稀疏的运动表示（由 kp_source 和 kp_driving 提供）中预测密集的运动。
+    主要用于人脸渲染或视频动作迁移等任务中。
     """
 
     def __init__(self, block_expansion, num_blocks, max_features, num_kp, feature_channel, reshape_depth, compress,
                  estimate_occlusion_map=False):
         super(DenseMotionNetwork, self).__init__()
+        """
+        参数：
+        block_expansion：指定 Hourglass 模块中每个残差块（residual block）的通道扩展系数。
+        num_blocks：指定 Hourglass 模块中堆叠的残差块的数量。
+        max_features：指定 Hourglass 模块中最大的通道数。
+        num_kp：表示关键点（keypoints）的数量。这是模型中要处理的关键点的个数
+        feature_channel：表示输入特征的通道数
+        reshape_depth：用于计算遮挡图的通道深度。
+        estimate_occlusion_map:一个标志，表示是否估计遮挡图。如果设置为 True，则会添加用于估计遮挡图的卷积层，否则为 None
+        """
+        """
+        初始化模块：
+        self.hourglass：堆叠的 Hourglass 网络模块，用于处理输入特征。
+        self.mask：卷积层，输出一个用于加权运动的遮罩。
+        self.compress：卷积层，用于压缩输入特征。
+        self.norm：批归一化层，对压缩后的特征进行归一化。
+        self.occlusion：如果 estimate_occlusion_map 为 True，则为用于估计遮挡图的卷积层，否则为 None。
+        self.num_kp：记录了关键点的数量，以备后续使用。
+        """
         # self.hourglass = Hourglass(block_expansion=block_expansion, in_features=(num_kp+1)*(feature_channel+1), max_features=max_features, num_blocks=num_blocks)
         self.hourglass = Hourglass(block_expansion=block_expansion, in_features=(num_kp+1)*(compress+1), max_features=max_features, num_blocks=num_blocks)
 
@@ -30,7 +51,7 @@ class DenseMotionNetwork(nn.Module):
 
         self.num_kp = num_kp
 
-
+# 利用稀疏运动表示，对输入特征进行变形。通过使用 F.grid_sample 函数，实现了在特征上的空间变形。
     def create_sparse_motions(self, feature, kp_driving, kp_source):
         bs, _, d, h, w = feature.shape
         identity_grid = make_coordinate_grid((d, h, w), type=kp_source['value'].type())
@@ -56,6 +77,7 @@ class DenseMotionNetwork(nn.Module):
 
         return sparse_motions
 
+# 创建用于网络输入的热图表示。通过计算 kp_driving 和 kp_source 的高斯热图之差。
     def create_deformed_feature(self, feature, sparse_motions):
         bs, _, d, h, w = feature.shape
         feature_repeat = feature.unsqueeze(1).unsqueeze(1).repeat(1, self.num_kp+1, 1, 1, 1, 1, 1)      # (bs, num_kp+1, 1, c, d, h, w)
@@ -78,6 +100,12 @@ class DenseMotionNetwork(nn.Module):
         return heatmap
 
     def forward(self, feature, kp_driving, kp_source):
+        """
+        将输入特征进行压缩、正则化，并构建了热图表示。
+        使用 Hourglass 模块处理这些输入，输出一个遮罩 mask。
+        遮罩 mask 通过 softmax 操作，用于加权运动。
+        如果需要，通过卷积层 occlusion 计算遮挡图。
+        """
         bs, _, d, h, w = feature.shape
 
         feature = self.compress(feature)
