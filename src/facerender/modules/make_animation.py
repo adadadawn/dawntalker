353c86f935d586a52bd6e658379747acd0cfedb4
FengAB2 <115358776+FengAB2@users.noreply.github.com>
2023-11-24 20:02:58 +0800

    make_animation.py
    feng

--- src/facerender/modules/make_animation.py
+++ src/facerender/modules/make_animation.py
@@ -3,9 +3,19 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 from tqdm import tqdm 
+"""
+这段代码实现了一个用于生成动画的模型，
+"""
 
+
+# 这个函数用于规范化关键点（keypoints）。
 def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale=False,
                  use_relative_movement=False, use_relative_jacobian=False):
+    """
+    如果adapt_movement_scale为True，它会根据源关键点和驱动关键点的凸包面积比例来调整运动的尺度。
+    如果use_relative_movement为True，它将根据相对运动来调整关键点的位置。
+    如果use_relative_jacobian为True，它将根据相对雅可比矩阵来调整关键点的位置。
+    """
     if adapt_movement_scale:
         source_area = ConvexHull(kp_source['value'][0].data.cpu().numpy()).volume
         driving_area = ConvexHull(kp_driving_initial['value'][0].data.cpu().numpy()).volume
@@ -27,6 +37,9 @@ def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale
     return kp_new
 
 def headpose_pred_to_degree(pred):
+    """
+    这个函数将头部姿态的预测值转换为角度值。
+    """
     device = pred.device
     idx_tensor = [idx for idx in range(66)]
     idx_tensor = torch.FloatTensor(idx_tensor).type_as(pred).to(device)
@@ -34,7 +47,11 @@ def headpose_pred_to_degree(pred):
     degree = torch.sum(pred*idx_tensor, 1) * 3 - 99
     return degree
 
+
 def get_rotation_matrix(yaw, pitch, roll):
+    """
+    这个函数根据给定的yaw、pitch和roll角度，计算出旋转矩阵。
+    """
     yaw = yaw / 180 * 3.14
     pitch = pitch / 180 * 3.14
     roll = roll / 180 * 3.14
@@ -62,7 +79,13 @@ def get_rotation_matrix(yaw, pitch, roll):
 
     return rot_mat
 
+
 def keypoint_transformation(kp_canonical, he, wo_exp=False):
+    """
+
+    这个函数对关键点进行变换，考虑到头部姿态和表情的影响。
+    get_rotation_matrix用于计算旋转矩阵，headpose_pred_to_degree用于获取角度。
+    """
     kp = kp_canonical['value']    # (bs, k, 3) 
     yaw, pitch, roll= he['yaw'], he['pitch'], he['roll']      
     yaw = headpose_pred_to_degree(yaw) 
@@ -103,6 +126,18 @@ def make_animation(source_image, source_semantics, target_semantics,
                             generator, kp_detector, he_estimator, mapping, 
                             yaw_c_seq=None, pitch_c_seq=None, roll_c_seq=None,
                             use_exp=True, use_half=False):
+    """
+    这个函数用于生成动画。
+    部分参数：
+    source_semantics：源图像的面部语义信息，通常是一个张量，包含源图像中与面部特征相关的语义信息。
+    target_semantics：目标图像的面部语义信息，也是一个张量，包含目标图像中与面部特征相关的语义信息。
+    这些语义信息可以包括对面部特征的描述，例如面部表情、姿势等。
+    在生成动画的过程中，模型会根据这些语义信息调整源图像的关键点和生成目标图像。
+    步骤：
+    对于每一帧，首先计算源关键点的规范化版本，然后根据目标语义生成相应的头部姿态。
+    使用规范化的源关键点和生成的头部姿态作为输入，通过生成器（generator）生成预测结果。
+    返回所有帧的预测结果。
+    """
     with torch.no_grad():
         predictions = []
 
@@ -141,6 +176,8 @@ def make_animation(source_image, source_semantics, target_semantics,
 class AnimateModel(torch.nn.Module):
     """
     Merge all generator related updates into single model for better multi-gpu usage
+    将所有与生成器相关的更新合并到单个模型中，以更好地使用多gpu
+    这个类将关键点提取器（kp_extractor）、生成器（generator）和映射器（mapping）封装在一起。
     """
 
     def __init__(self, generator, kp_extractor, mapping):
@@ -154,7 +191,9 @@ class AnimateModel(torch.nn.Module):
         self.mapping.eval()
 
     def forward(self, x):
-        
+        """
+        在前向传播过程中，它接收源图像、源语义、目标语义以及头部姿态的序列，然后调用make_animation函数生成动画序列。
+        """
         source_image = x['source_image']
         source_semantics = x['source_semantics']
         target_semantics = x['target_semantics']
